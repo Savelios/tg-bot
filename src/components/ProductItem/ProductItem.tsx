de09d3a71fe20a217811344987e1e221f630bad2
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-10-19 16:03:49 +0300

    add

---
+++ src/components/ProductItem/ProductItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '../Button/Button';
+import './ProductItem.css';
+
+interface Product {
+  title: string;
+  description: string;
+  price: number;
+  imageSrc: string;
+}
+
+interface ProductItemProps {
+  product: Product;
+  className?: string;
+  onAdd: (product: Product) => void;
+}
+
+const ProductImage: React.FC<{ src: string }> = ({ src }) => (
+  <div className="img" style={{ backgroundImage: `url(${src})` }} />
+);
+
+const ProductItem: React.FC<ProductItemProps> = ({ product, className = '', onAdd }) => {
+  const onAddHandler = () => {
+    onAdd(product);
+  };
+
+  return (
+    <div className={`product ${className}`}>
+      <ProductImage src={product.imageSrc} />
+      <div className="title">{product.title}</div>
+      <div className="description">{product.description}</div>
+      <div className="price">
+        <span>Стоимость: <b>{product.price}</b></span>
+      </div>
+      <Button className="add-btn" onClick={onAddHandler}>
+        Добавить в корзину
+      </Button>
+    </div>
+  );
+};
+
+export default ProductItem;
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,10 +1,104 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import Button from "../Button/Button";
 import "./ProductList.css";
+import ProductItem from "../ProductItem/ProductItem";
+import { useTelegram } from "../../hooks/useTelegram";
+import jeans from "../../assets/img/jeans.jpeg";
 
-const ProductList = () => {
-  return <div>
-    ProductList
-  </div>;
+interface Product {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  imageSrc: string;
+}
+
+const products: Product[] = [
+  {
+    id: "1",
+    title: "Джинсы",
+    price: 5000,
+    description: "Синего цвета, прямые",
+    imageSrc: jeans,
+  },
+  {
+    id: "2",
+    title: "Шорты",
+    price: 9000,
+    description: "Темного цвета, прямые",
+    imageSrc: jeans,
+  },
+];
+
+const getTotalPrice = (items: Product[] = []): number => {
+  return items.reduce((acc, item) => acc + item.price, 0);
+};
+
+const sendProductData = (data: any, queryId: string) => {
+  fetch("http://85.119.146.179:8000/web-data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+};
+
+const ProductList: React.FC = () => {
+  const [addedItems, setAddedItems] = useState<Product[]>([]);
+  const { tg, queryId } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId,
+    };
+
+    sendProductData(data, queryId);
+  }, [addedItems, queryId]);
+
+  useEffect(() => {
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      tg.offEvent("mainButtonClicked", onSendData);
+    };
+  }, [onSendData, tg]);
+
+  const onAdd = (product: Product) => {
+    const alreadyAdded = addedItems.find((item) => item.id === product.id);
+    let newItems: Product[] = [];
+
+    if (alreadyAdded) {
+      newItems = addedItems.filter((item) => item.id !== product.id);
+    } else {
+      newItems = [...addedItems, product];
+    }
+
+    setAddedItems(newItems);
+
+    if (newItems.length === 0) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `Купить ${getTotalPrice(newItems)}`,
+      });
+    }
+  };
+
+  return (
+    <div className="list">
+      {products.map((item) => (
+        <ProductItem
+          key={item.id}
+          product={item}
+          onAdd={onAdd}
+          className="item"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
